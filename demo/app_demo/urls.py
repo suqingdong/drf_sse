91638aa33e42c11e65c3a8bba1ac9ecd6735bccc
suqingdong <1078595229@qq.com>
2024-04-02 10:41:45 +0800

    update .gitignore

--- demo/app_demo/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path, include
-
-from .views import MyView
-
-
-urlpatterns = [
-    path('', MyView.as_view(), name='myview'),
-]
--- drf_sse/mixins.py
+++
@@ -1,9 +0,0 @@
-from .renderers import SSERenderer
-
-
-class SSEMixin(object):
-
-    def get_renderers(self):
-        if 'text/event-stream' in self.request.META.get('HTTP_ACCEPT', ''):
-            return [SSERenderer()]
-        return super().get_renderers()
